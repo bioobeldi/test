d76f3d17ecf251c1e9ff174f1f411583040c3a8a
bioobeldi <139604663+bioobeldi@users.noreply.github.com>
2023-07-19 06:28:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-  function changeLanguage() {
-    var selectedLang = document.getElementById("lang-select").value;
-  
-    // Redirect to the appropriate page with the language parameter
-    if (selectedLang === "fr") {
-      window.location.href = "page-fr.html";
-    } else if (selectedLang === "en") {
-      window.location.href = "page-en.html";
-    } else if (selectedLang === "ar") {
-      window.location.href = "page-ar.html";
-    }
-  }
-  
-  
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuLeft = document.querySelector('.menu-left');
-const menuRight = document.querySelector('.menu-right');
+// JavaScript complet pour le comportement du menu mobile
+const links = document.querySelectorAll('nav li');
 
-hamburgerMenu.addEventListener('click', () => {
-  // Lorsqu'on clique sur le bouton hamburger
-  menuLeft.classList.toggle('menu-mobile'); // Afficher/masquer le menu gauche
-  menuRight.classList.toggle('menu-mobile'); // Afficher/masquer le menu droit
+icons.addEventListener("click",() => {
+  nav.classList.toggle("active")
+});
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  })
 });
 
-// masquer les menus lorsque l'utilisateur clique ailleurs sur la page.
-document.addEventListener('click', (event) => {
-  const targetElement = event.target;
-  if (!targetElement.closest('.hamburger-menu') && !targetElement.closest('.menu-mobile')) {
-    menuLeft.classList.remove('menu-mobile');
-    menuRight.classList.remove('menu-mobile');
-  }
-});
+function changeLanguage() {
+  var selectedLang = document.getElementById("lang-select").value;
 
-  
+  // Redirect to the appropriate page with the language parameter
+  if (selectedLang === "fr") {
+    window.location.href = "page-fr.html";
+  } else if (selectedLang === "en") {
+    window.location.href = "page-en.html";
+  } else if (selectedLang === "ar") {
+    window.location.href = "page-ar.html";
+  }
+}
